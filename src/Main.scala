9a951710fe8d46f223100677c1066b4064c3b6da
lzj <509649444@163.com>
2015-10-04 15:01:28 +0800

    01

--- src/Main.scala
+++ src/Main.scala
@@ -1,8 +1,15 @@
 /**
  * Created by lzj on 15-10-4.
  */
+// P01: Find the last element of a list.
+object Scala99{
+  def mlast[A](xs: List[A]) = xs.last
+}
+
+
+
 object Main {
   def main(args: Array[String]) {
-    
+    println(Scala99.mlast(List(1,2,3)))
   }
 }
